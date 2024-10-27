547715a2b1465e18c4a007b539bab0f32897f500
dtahos <dimitristahos@gmal.com>
2024-10-27 11:35:37 +0200

    sac

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello, World! shit\n");
+  res.end("Hello, World! shi sxdcacascst\n");
 });
 
 server.listen(port, hostname, () => {
