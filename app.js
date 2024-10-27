be05e11e5af9eadae67ae43268fa3d1d2e7f13a6
dtahos <dimitristahos@gmal.com>
2024-10-27 11:38:15 +0200

    exit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello, World! shi sxdcacascst\n");
+  res.end("Hello, World! shi sxdcacascst exit\n");
 });
 
 server.listen(port, hostname, () => {
